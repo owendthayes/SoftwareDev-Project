62747ae7e92e465e665773b7877e17d92f054605
broy9501 <64872666+broy9501@users.noreply.github.com>
2024-03-17 13:43:39 +0000

    Update add_user_to_group.php

--- WebPageProd/php/add_user_to_group.php
+++ WebPageProd/php/add_user_to_group.php
@@ -38,19 +38,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if ($isMember) {
             echo "User is already a member of the group.";
         } else {
-            // Add the user to group_participants
-            $addUserQuery = "INSERT INTO group_participants (groupid, username, gpermissions, fpermissions) VALUES (?, ?, 'member', 'viewer')";
-            $addUserStmt = mysqli_prepare($connection, $addUserQuery);
-            mysqli_stmt_bind_param($addUserStmt, 'is', $groupid, $userSearch);
-            $addResult = mysqli_stmt_execute($addUserStmt);
-            mysqli_stmt_close($addUserStmt);
+            // Begin transaction
+            mysqli_begin_transaction($connection);
+            try {
+                // Add the user to group_participants
+                $addUserQuery = "INSERT INTO group_participants (groupid, username, gpermissions, fpermissions) VALUES (?, ?, 'member', 'viewer')";
+                $addUserStmt = mysqli_prepare($connection, $addUserQuery);
+                mysqli_stmt_bind_param($addUserStmt, 'is', $groupid, $userSearch);
+                $addResult = mysqli_stmt_execute($addUserStmt);
+                mysqli_stmt_close($addUserStmt);
 
-            if ($addResult) {
-                // header("Location: ../groupview2.php");
-                header("Location: " . $_SERVER['HTTP_REFERER']);
-                // echo "User added successfully to the group.";
-            } else {
-                echo "Error adding user to the group: " . mysqli_error($connection);
+                if ($addResult) {
+                    // Get group name for notification content
+                    $groupQuery = "SELECT groupname FROM groups WHERE groupid = ?";
+                    $groupStmt = mysqli_prepare($connection, $groupQuery);
+                    mysqli_stmt_bind_param($groupStmt, 'i', $groupid);
+                    mysqli_stmt_execute($groupStmt);
+                    $groupResult = mysqli_stmt_get_result($groupStmt);
+                    $group = mysqli_fetch_assoc($groupResult);
+                    $groupName = $group['groupname'] ?? 'a group';
+                    mysqli_stmt_close($groupStmt);
+
+                    // Insert notification that user was added to the group
+                    $notificationQuery = "INSERT INTO notifications (recipient_username, sender_username, activity_type, object_type, object_id, content, time_sent) VALUES (?, ?, 'group_join', 'group', ?, CONCAT('You have been added to the group ', ?), CURRENT_TIMESTAMP())";
+                    $notificationStmt = mysqli_prepare($connection, $notificationQuery);
+                    mysqli_stmt_bind_param($notificationStmt, 'ssis', $userSearch, $_SESSION['username'], $groupid, $groupName);
+                    mysqli_stmt_execute($notificationStmt);
+                    mysqli_stmt_close($notificationStmt);
+
+                    mysqli_commit($connection);
+                    header("Location: " . $_SERVER['HTTP_REFERER']);
+                } else {
+                    throw new Exception("Error adding user to the group: " . mysqli_error($connection));
+                }
+            } catch (Exception $e) {
+                mysqli_rollback($connection);
+                echo $e->getMessage();
             }
         }
     } else {
