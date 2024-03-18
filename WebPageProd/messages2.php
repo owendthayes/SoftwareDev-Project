334f8a5e50a9df7dbbbfdcdc827a96e00068d499
broy9501 <64872666+broy9501@users.noreply.github.com>
2024-03-18 11:47:53 +0000

    Update messages2.php

--- WebPageProd/messages2.php
+++ WebPageProd/messages2.php
@@ -21,17 +21,16 @@ session_start();
         <nav>
             <div class="mainnav">
                 <div class="imgnav">
-                    <img src="Images/logo.png">
+                    <a href="../home.html"><img src="Images/logo.png"></a>
                     <div class="compname">
                         <h1 style="color:white;">CreativSync</h1>
                     </div>
                 </div>
                 <div class="navi">
-                    <a href="feed.html">Feed</a>
-                    <a href="notifications.html">Notifications</a>
-                    <a href="search.html" id="showSearch">Search</a>
-                    <a href="groups.html">Groups</a>
-                    <a href="help.html">Help</a>
+                    <a href="feed.php">Feed</a>
+                    <a href="notifications.php">Notifications</a>
+                    <a href="#" id="showSearch">Search</a>
+                    <a href="groupsFiles.php">Groups</a>
                     <button><a href="profile.php">Profile</a></button>
                 </div>
             </div>
