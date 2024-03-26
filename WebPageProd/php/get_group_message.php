287de57a75c7db24a8979af42898b11a91978bf3
broy9501 <64872666+broy9501@users.noreply.github.com>
2024-03-26 12:59:33 +0000

    Update get_group_message.php

--- WebPageProd/php/get_group_message.php
+++ WebPageProd/php/get_group_message.php
@@ -9,46 +9,61 @@ $query = "SELECT * FROM messages WHERE groupid = ? ORDER BY timestamp";
 $stmt = mysqli_prepare($connection, $query);
 
 if ($stmt) {
-
     mysqli_stmt_bind_param($stmt, "s", $groupID);
     mysqli_stmt_execute($stmt);
 
     $result = mysqli_stmt_get_result($stmt);
     $output = "";
 
-    if ($stmt) {
-        mysqli_stmt_bind_param($stmt, "s", $groupID);
-        mysqli_stmt_execute($stmt);
-        $result = mysqli_stmt_get_result($stmt);
-
+    if ($result) {
         while ($row = mysqli_fetch_assoc($result)) {
             if ($row['sender'] == $username) {
-                $output .= '<div class="message my-message">
+                if (strpos($row['content_text'], '../Images') === 0) {
+                    // If it's an image, display it as an <img> tag
+                    $imagePath = str_replace('../Images', 'Images', $row['content_text']);
+                    $output .= '<div class="message my-message">
+                                    <div class="myMessage">
+                                        <img src="' . $imagePath . '" alt="Image" class="message-image">
+                                        <p class="messageDate">' . $row['timestamp'] . '</p>
+                                    </div>
+                                </div>';
+                } else {
+                    $output .= '<div class="message my-message">
                                     <img src="Images/defaultAvatar.png" alt="My Profile" class="profile-pic">
                                     <p class="message-content">' . $row['content_text'] . '</p>
                                 </div>';
+                }
             } else {
-                $output .= '<div class="message their-message">
+                if (strpos($row['content_text'], '../Images') === 0) {
+                    // If it's an image, display it as an <img> tag
+                    $imagePath = str_replace('../Images', 'Images', $row['content_text']);
+                    $output .= '<div class="message their-message">
+                                    <div class="theirMessage">
+                                        <img src="' . $imagePath . '" alt="Image" class="message-image">
+                                        <p class="messageDate">' . $row['timestamp'] . '</p>
+                                    </div>
+                                </div>';
+                } else {
+                    $output .= '<div class="message their-message">
                                     <div class="theirMessage">
                                         <img src="Images/defaultAvatar.png" alt="User Profile" class="profile-pic">
                                         <div class="message-content-container">
-                                        <span class="user-name">'. $row['sender'] .'</span>
-                                        <p class="message-content">' . $row['content_text'] . '</p>
+                                            <span class="user-name">' . $row['sender'] . '</span>
+                                            <p class="message-content">' . $row['content_text'] . '</p>
                                         </div>
                                         <p class="messageDate">' . $row['timestamp'] . '</p>
                                     </div>
                                 </div>';
+                }
             }
         }
         echo $output;
     } else {
-        return "Error: " . mysqli_error($connection);
+        echo "Error fetching messages.";
     }
-    mysqli_stmt_close($stmt);
-
-    // Return chatID
-    return $output;
 } else {
-    return "Error preparing statement: " . mysqli_error($connection);
+    echo "Error preparing statement: " . mysqli_error($connection);
 }
+
+mysqli_close($connection);
 ?>
